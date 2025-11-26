1a8ec637286346960b4dbafc413e809a7144fa01
Regina734 <reginamorenikequenum@gmail.com>
2025-11-26 11:00:07 +0100

    First Homepage

---
+++ android/app/src/main/kotlin/com/example/adjo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.adjo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+//import 'package:adjocoin/pageAcceuil/acceuille1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+      //AcceuillePage1(),
+    );
+  }
+}
